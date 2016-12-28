7529388d6fc933ae3bf0bbce749b980e8ff0ebae
Ralle1968 <graef.ralf@gmail.com>
2016-12-28 21:47:25 +0100

    vuelaravel

---
+++ src/components/authentication/Login.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="form-group">
+						<input v-model="email" type="email" placeholder="Email"	class="form-control">
+					</div>
+
+					<div class="form-group">
+						<input type="password" placeholder="Passwort" v-model="password"	class="form-control">
+					</div>
+
+					<button @click='login' class="btn btn-success pull-right">Login</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+
+<script>
+	export default {
+		data () {
+			return {
+				email:'',
+				password:''
+			}
+		},
+
+		methods: {
+			login() {
+				var data = {
+					client_id: 2,
+					client_secret: 'ZfiiiFZou3S5vKF9eIRIXSF58x3jqPdGzb3zTtLL',
+					grant_type:'password',
+					username: this.email,
+					password: this.password
+				}
+
+				this.$http.post('http://localhost:8000/oauth/token', data).then (function(response){
+					this.$auth.setToken(response.body.access_token, response.body.expires_in + Date.now())
+				})
+			}
+		}
+	}
+</script>
+
+
+
+
+<style>
+	
+</style>
